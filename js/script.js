aaf4cc7104f0454b7639c6d359d8dfd1674fffee
Johannes Balthasar <johannes.balthasar@learnet.se>
2026-01-09 15:52:34 +0100

    Lov + idag

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 // Denna fil skall innehålla JavaScript (JS)
+
+const stickOffset = 1 //Offset from nav top positon to start sticking the element
+const positionOffset = -5 //Positon offset of the element when sticking
+
+const nav = document.querySelector('nav')
+
+document.body.onscroll = () => {
+    if (document.body.clientWidth >= 600 && window.scrollY > nav.scrollHeight + stickOffset - positionOffset) {
+        nav.style.position = 'fixed'
+        nav.style.height = 'calc(11%)'
+        nav.style.transform = 'translate(0, ' + positionOffset + 'px)'
+    } else if (document.body.clientWidth < 600 || window.scrollY <= nav.scrollHeight + stickOffset) {
+        nav.style.position = 'static'
+        nav.style.height = null
+    }
+}
+
+const contactForm = document.getElementById('contact')
+const inputs = document.querySelectorAll('input,textarea')
+
+inputs.forEach((input) => {
+    input.onchange = () => {
+        if (input.value && input.value != '') {
+            input.setAttribute('value', input.value)
+        } else {
+            input.removeAttribute('value')
+        }
+    }
+})
