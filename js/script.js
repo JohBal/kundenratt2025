9dcae67f2c5ecc0513cf3f1a080f54eb7a97f770
JohBal <214463964+JohBal@users.noreply.github.com>
2026-01-18 22:26:47 +0100

    Sista inlämning script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 // Denna fil skall innehålla JavaScript (JS)
 
-const stickOffset = 40 //Offset from nav top positon to start sticking the element
+const stickOffset = 1 //Offset from nav top positon to start sticking the element
 const positionOffset = -5 //Positon offset of the element when sticking
 
 const nav = document.querySelector('nav')
+const topHeader = document.querySelector('.header-top')
 
 document.body.onscroll = () => {
-    if (document.body.clientWidth >= 600 && window.scrollY > nav.scrollHeight + stickOffset - positionOffset) {
+    if (document.body.clientWidth >= 600 && window.scrollY > topHeader.scrollHeight + stickOffset - positionOffset) {
         nav.style.position = 'fixed'
         nav.style.height = '11%'
         nav.style.transform = 'translate(0, ' + positionOffset + 'px)'
-    } else if (document.body.clientWidth < 600 || window.scrollY <= nav.scrollHeight + stickOffset) {
+    } else if (document.body.clientWidth < 600 || window.scrollY <= topHeader.scrollHeight + stickOffset) {
         nav.style.position = 'static'
-        nav.style.height = null
+        nav.style.removeProperty('height')
+        nav.style.removeProperty('transform')
     }
 }
 
